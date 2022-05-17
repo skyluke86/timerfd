5cafcef9804c6a1940b177190ace0f5eefd10034
lc <luca.castelli@skycommitalia.it>
2022-05-17 10:42:54 +0200

    Added LICENSE and README

--- src/test.c
+++ src/test.c
@@ -1,8 +1,29 @@
 /*
- * test.c
- *
  *  Created on: May 16, 2022
  *      Author: Luca Castelli
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License (not later!)
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not,  see <http://www.gnu.org/licenses>
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * test.c
+ *
+ * This program is used to test the timerfd. The software creates and timer
+ * file descriptor, it assigns a timeout and a callback to verify
+ * the timeout frequency. It has been tested on a ARM cortex A53
+ *
  */
 #include <stdio.h>
 #include <time.h>
